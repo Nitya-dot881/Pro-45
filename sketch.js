36510499315e21b800e9c897dc69e1e500529c53
Nitya-dot881 <79143712+Nitya-dot881@users.noreply.github.com>
2021-07-08 07:37:09 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var thief
+var police,trains,ground
+var gold;
+
+function preload()
+{
+  bg=loadImage("bacground.png")
+}
+
+
+function setup()
+{
+createCanvas(windowWidth,windowHeight)
+
+
+
+}
+function draw()
+{
+   background(bg)
+  
+}
